6b5ac180fee265f8fecd5918ceb85213abaafc62
sandeepanainala <nainala_sandeep@yahoo.com>
2025-07-20 13:02:05 +0530

    terraform vpc eks

--- EKS-cluster/main.tf
+++ EKS-cluster/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "devops_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    name = "devops_vpc"
+    name = "devops-vpc"
   }
   
 }
@@ -15,5 +15,72 @@ resource "aws_subnet" "devops_subnet" {
 
   tags = {
     name = "devops_subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "devops_igw" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    name = "devops-igw"
+  }
+}
+
+resource "aws_route_table" "devops_route_table" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.devops_igw.id
+  }
+    tags = {
+        name = "devops-route-table"
+    }
+}
+
+resource "aws_route_table_association" "devops_route_table_association" {
+  subnet_id      = aws_subnet.devops_subnet.id
+  route_table_id = aws_route_table.devops_route_table.id
+  count = length(aws_subnet.devops_subnet.*.id)
+  
+}
+
+resource "aws_security_group" "devops_cluster_sg" {
+  name        = "devops-sg"
+  description = "Security group for devops resources"
+  vpc_id      = aws_vpc.devops_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    name = "devops-cluster-sg"
+  }
+}
+
+resource "aws_security_group" "devops_node_sg" {
+  name        = "devops-sg"
+  description = "Security group for devops resources"
+  vpc_id      = aws_vpc.devops_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "devops-node-sg"
   }
 }
