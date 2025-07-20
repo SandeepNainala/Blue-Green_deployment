d851abdf79a5b9342da09aa2add14192dcc44f71
sandeepanainala <nainala_sandeep@yahoo.com>
2025-07-20 16:39:30 +0530

    eks terraform

--- EKS-cluster/main.tf
+++ EKS-cluster/main.tf
@@ -8,6 +8,7 @@ resource "aws_vpc" "devops_vpc" {
 }
 
 resource "aws_subnet" "devops_subnet" {
+  count             = 2
   vpc_id            = aws_vpc.devops_vpc.id
   cidr_block        = cidrsubnet(aws_vpc.devops_vpc.cidr_block, 8, count.index)
   availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
